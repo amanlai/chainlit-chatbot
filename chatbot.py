79699b4fdc787e5a93e226efccfeaa9fc5c548bc
amanlai <manlai.lp@gmail.com>
2024-02-24 23:40:17 -0800

    bare backbone

---
+++ chatbot.py
@@ -0,0 +1,325 @@
+import os
+from datetime import datetime
+from tempfile import NamedTemporaryFile
+# from functools import partial
+import streamlit as st
+from streamlit_chat import message
+from langchain_community.chat_models import ChatOpenAI
+# from langchain_community.vectorstores import Chroma
+# from langchain.prompts import ChatPromptTemplate#, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.agents import tool, AgentExecutor, create_openai_tools_agent
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain.tools.retriever import create_retriever_tool
+# from langchain.agents.agent_toolkits.conversational_retrieval.openai_functions import create_conversational_retrieval_agent
+# from langchain.chains import ConversationalRetrievalChain
+# from langchain.agents.format_scratchpad import format_to_openai_function_messages
+# from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+# from langchain_community.tools.convert_to_openai import format_tool_to_openai_function
+# from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+from lib.tools import get_tools
+from ingest import IngestData
+# import psycopg2
+import pymongo
+
+load_dotenv()
+
+persist_directory = os.environ.get("PERSIST_DIRECTORY", './db')
+MESSAGE_PROMPT = "Ask me anything!"
+SYSTEM_TEMPLATE = "You are a helpful bot. If you do not know the answer, just say that you do not know, do not try to make up an answer."
+os.environ['OPENAI_API_KEY'] = os.environ.get('OPENAI_API_KEY', 'dummy_key')
+model_name = os.environ.get("MODEL_NAME", 'gpt-3.5-turbo')
+use_client = os.environ.get("USE_CLIENT", 'True') == 'True'
+
+
+
+
+# # Initialize connection.
+# # Uses st.cache_resource to only run once.
+# @st.cache_resource
+# def init_connection():
+#     return pymongo.MongoClient(**st.secrets["mongo"])
+
+# print(st.secrets['mongo'])
+# client = init_connection()
+
+# # Pull data from the collection.
+# # Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+# def get_data():
+#     db = client.Hyatt_Mall
+#     items = db.hyatt_mall_data.find()
+#     items = list(items)  # make hashable for st.cache_data
+#     return items
+
+# items = get_data()
+
+# # Print results.
+# for item in items:
+#     st.write(f"{item['name']} has a :{item['pet']}:")
+
+
+uri = "mongodb+srv://amanlai:Qlj9UkaNwtrJxNlF@streamlit-chatbot.uyfl3sn.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = pymongo.mongo_client.MongoClient(uri, server_api=pymongo.server_api.ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+############################################################
+##################### POSTGRES #############################
+############################################################
+
+# # Define database credentials here
+# DB_HOST = "db"
+# DB_PORT = 5432
+# DB_NAME = "chatbot_db"
+# DB_USER = "your_username"
+# DB_PASSWORD = "your_password"
+
+
+# def get_configuration_data(
+#         host=DB_HOST, 
+#         port=DB_PORT, 
+#         dbname=DB_NAME, 
+#         user=DB_USER, 
+#         password=DB_PASSWORD
+# ):
+#     try:
+#         conn = psycopg2.connect(
+#             host=host,
+#             port=port,
+#             dbname=dbname,
+#             user=user,
+#             password=password
+#         )
+#         cursor = conn.cursor()
+#         cursor.execute("SELECT query, answer FROM examples")
+#         rows = cursor.fetchall()
+#         examples = [{"query": question, "answer": answer} for (question, answer) in rows]
+#         cursor.close()
+#         conn.close()
+#         return examples
+#     except psycopg2.Error as e:
+#         raise Exception(f"Error connecting to the database: {e}")
+ 
+
+
+# # Connect to the PostgreSQL database and retrieve examples
+# examples = get_configuration_data(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD)
+
+
+
+
+def build_tools(vector_store, k=5):
+    """
+    Creates the list of tools to be used by the Agent Executor
+    """
+    retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": k})
+
+    retriever_tool = create_retriever_tool(
+        retriever,
+        "search_document",
+        """Searches and retrieves information from the vector store to answer questions whose answers can be found there.""",
+    )
+
+    st.session_state['tools'] = [*get_tools(), retriever_tool]
+
+
+def build_data(uploaded_file, data_processor):
+    """
+    Process data on the sidebar
+    """
+    with st.spinner('Reading, splitting and embedding file...'):
+
+        # writing the file from RAM to a temporary file that is deleted later
+        with NamedTemporaryFile(delete=False) as tmp:
+            # ext = os.path.splitext(uploaded_file.name)[1]
+            tmp.write(uploaded_file.read())
+            vector_store = data_processor.build_embeddings(tmp.name)
+            # vector_store = generate_index(tmp.name, api_key)
+
+        os.remove(tmp.name)
+        st.success('File uploaded, chunked and embedded successfully.')
+        return vector_store
+
+
+def clear():
+    if os.environ['OPENAI_API_KEY'] == 'dummy_key':
+        os.environ['OPENAI_API_KEY'] = st.session_state['openai_key']
+        st.session_state['openai_key'] = ""
+
+
+
+def create_answer():
+
+    # creating a reply
+    question = st.session_state['question']
+    chat_history = st.session_state['chat_history']
+    agent_executor = st.session_state['agent_executor']
+
+    result = agent_executor.invoke({"input": question, "chat_history": chat_history})
+    answer = result['output']
+    st.session_state['chat_history'].extend((HumanMessage(content=question), AIMessage(content=answer)))
+    st.session_state['question'] = ""
+
+
+def create_agent(temperature, system_message):
+
+    sys_msg = f"""You are a helpful assistant. Respond to the user as helpfully and accurately as possible.
+
+    It is important that you provide an accurate answer. If you're not sure about the details of the query, don't provide an answer; ask follow-up questions to have a clear understanding.
+
+    Use the provided tools to perform calculations and lookups related to the calendar and datetime computations.
+
+    If you don't have enough context to answer question, you should ask user a follow-up question to get needed info. 
+    
+    Always use tools if you have follow-up questions to the request or if there are questions related to datetime.
+    
+    For example, given question: "What time will the restaurant open tomorrow?", follow the following steps to answer it:
+    
+      1. Use get_day_of_week tool to find the week day name of tomorrow.
+      2. Use search_document tool to see if the restaurant is open on that week day name.
+      3. The restaurant might be closed on specific dates such as a Christmas Day, therefore, use get_date tool to find calendar date of tomorrow.
+      4. Use search_document tool to see if the restaurant is open on that date.
+      5. Generate an answer if possible. If not, ask for clarifications.
+    
+
+    Don't make any assumptions about data requests. For example, if dates are not specified, you ask follow up questions. 
+    There are only two assumptions you can make about a query:
+    
+      1. if the question is about dates but no year is given, you can assume that the year is {datetime.today().year}.
+      2. if the question includes a weekday, you can assume that the week is the calendar week that includes the date {datetime.today().strftime('%m-%d-%Y')}.
+
+    
+
+    Dates should be in the format mm-dd-YYYY.
+
+    
+    
+    {system_message}
+
+
+    
+    If you can't find relevant information, instead of making up an answer, say "Let me connect you to my colleague".
+    """
+
+    llm = ChatOpenAI(model=model_name, temperature=temperature)
+    tools = st.session_state.pop('tools')
+
+    # system_message = SystemMessage(content=sys_msg)
+    # agent_executor = create_conversational_retrieval_agent(
+    #     llm=llm, 
+    #     tools=tools, 
+    #     system_message=system_message,
+    #     verbose=True, 
+    #     max_token_limit=200
+    # )
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", sys_msg),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("user", "{input}"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
+    agent = create_openai_tools_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        max_iterations=10,
+        handle_parsing_errors=True,
+        early_stopping_method = 'generate'
+    )
+    
+    st.session_state['agent_executor'] = agent_executor
+
+
+
+
+
+
+def main():
+
+    # side bar
+    with st.sidebar:
+        # delete chat history
+        reset_chat = st.button("Start a new thread")
+        if reset_chat:
+            st.session_state['chat_history'] = []
+
+        st.text_input('OpenAI API Key:', type='password', key='openai_key', on_change=clear)
+        # api_key = True
+        # get any additional system message
+        system_message = st.text_input('System Message:')
+        # get the LLM model temperature
+        temperature = st.number_input('Temperature:', min_value=0., max_value=1., value=0.1)
+        # file uploader widget
+        col1, col2 = st.columns(2)
+        with col1:
+            uploaded_file = st.file_uploader('Upload a file:', type='pdf')
+        with col2:
+            for _ in range(10):
+                st.write('')
+            use_previously_saved_vector_store = st.checkbox('Use Previous File')
+
+        # add data button widget
+        add_data = st.button('Add Data')
+        # TODO: the uploaded file name is used as the collection name
+        if uploaded_file:
+            collection_name = 'chroma'
+            # collection_name = os.path.splitext(uploaded_file.name)[0].replace(" ", "")
+        else:
+            collection_name = 'chroma'
+        if add_data:
+            data_processor = IngestData(
+                api_key=os.environ['OPENAI_API_KEY'],
+                collection_name=collection_name,
+            )
+            if uploaded_file: # if the user browsed a file
+                vector_store = build_data(uploaded_file, data_processor)
+            elif use_previously_saved_vector_store:
+                vector_store = data_processor.get_vector_store()
+            else:
+                msg = "must either upload a file or click the button to use existing data"
+                st.write(msg)
+                raise ValueError(msg)
+            # saving the vector store in the streamlit session state (to be persistent between reruns)
+            st.session_state['vector_store'] = vector_store
+            build_tools(vector_store)
+            create_agent(temperature, system_message)
+
+
+
+    # main page
+    st.subheader('Your Chat Assistant')
+    
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+
+    if 'vector_store' in st.session_state:
+        # displaying the chat history
+        message("How may I assist you today?", is_user=False, key='🤖') # bot welcome text
+        if 'chat_history' in st.session_state:
+            for i, msg in enumerate(st.session_state['chat_history']):
+                if i % 2 == 0:
+                    message(msg.content, is_user=True, key=f'{i} 🤓')    # user's question
+                else:
+                    message(msg.content, is_user=False, key=f'{i} 🤖') # bot response
+            
+        # take question
+        st.text_input('Ask a question related to the document', on_change=create_answer, key='question')
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ ingest.py
@@ -0,0 +1,240 @@
+#! ../venv/Scripts/python.exe
+import os
+import glob
+# import boto3
+from multiprocessing import Pool
+from tqdm import tqdm
+from dotenv import load_dotenv
+import uuid
+from langchain_community.document_loaders import PyPDFLoader#, PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma, FAISS
+from langchain_openai import OpenAIEmbeddings
+import chromadb
+from chromadb.config import Settings
+
+
+load_dotenv()
+
+# Load environment variables
+os.environ['OPENAI_API_KEY'] = os.environ.get('OPENAI_API_KEY', 'dummy_key')
+persist_directory = os.environ.get("PERSIST_DIRECTORY", 'db')
+source_directory = os.environ.get("DOCUMENT_SOURCE_DIR", 'docs')
+verbose = os.environ.get("VERBOSE", 'True').lower() in ('true', '1', 't')
+# aws
+aws_enable = os.environ.get("AWS_ENABLE", 'False').lower() in ('true', '1', 't')
+aws_access_key = os.environ.get("AWS_ACCESS_KEY", "")
+aws_secret_key = os.environ.get("AWS_SECRET_KEY", "")
+aws_region = os.environ.get("AWS_REGION", "us-east-1")
+aws_bucket_name = os.environ.get("AWS_BUCKET_NAME", "")
+
+
+class CustomOpenAIEmbeddingFunction(OpenAIEmbeddings):
+
+    def __init__(self, openai_api_key, *args, **kwargs):
+        super().__init__(openai_api_key=openai_api_key, *args, **kwargs)
+        
+    def _embed_documents(self, texts):
+        return super().embed_documents(texts)
+
+    def __call__(self, input):
+        return self._embed_documents(input)
+
+
+
+class IngestData:
+
+    def __init__(
+            self, 
+            api_key=None, 
+            model_name="text-embedding-ada-002", 
+            database_type='faiss',
+            collection_name="chroma", 
+            host="localhost", 
+            port="8000", 
+            use_client=True,
+            chunk_size=256,
+            chunk_overlap=10,
+    ):
+        # embedding function to be used in collection
+        self.model_name = model_name
+        self.database_type = database_type
+        self.collection_name = collection_name
+        self.host = host
+        self.port = port
+        self.use_client = use_client
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+        if self.database_type == 'chroma' and self.use_client:
+            # self.db_client = chromadb.PersistentClient(
+            #     path=persist_directory, 
+            #     settings=Settings(allow_reset=True),
+            # )
+            # must run `chroma run --path db`` first
+            self.db_client = chromadb.HttpClient(
+                port=self.port,
+                host=self.host,
+                settings=Settings(allow_reset=True),
+            )
+            # define embedding model to be used for collections
+            self.embeddings = CustomOpenAIEmbeddingFunction(
+                openai_api_key=os.environ['OPENAI_API_KEY'] if api_key is None else api_key
+            )
+        else:
+            self.embeddings = OpenAIEmbeddings()
+
+
+
+    def load_document(self, filename):
+        """
+        Load PDF files as LangChain Documents
+        """
+        ext = "." + filename.rsplit(".", 1)[-1]
+        if ext != '.pdf':
+            raise ValueError(f"Unsupported file extension '{ext}'")
+
+        documents = []
+        loader = PyPDFLoader(filename)
+        try:
+            documents = loader.load()
+        except Exception as err:
+            print(f"ERROR: {filename}", err)
+        return documents
+    
+
+    def load_documents(self, source_dir, ignored_files=[]):
+        """
+        Loads all documents from the source documents directory, ignoring specified files
+        """
+        all_files = glob.glob(os.path.join(source_dir, "**/*.pdf"), recursive=True)
+        filtered_files = [file_path for file_path in all_files if file_path not in ignored_files]
+
+        with Pool(processes=os.cpu_count()) as pool:
+            results = []
+            with tqdm(total=len(filtered_files), desc='Loading new documents', ncols=80) as pbar:
+                for docs in pool.imap_unordered(self.load_document, filtered_files):
+                    results.extend(docs)
+                    pbar.update()
+        return results
+
+
+    def chunk_data(self):
+        """
+        Load the document, split it and return the chunks
+        """
+        # load document
+        documents = self.load_documents(source_directory)
+        if not documents:
+            print("No new documents to load")
+            exit(0)
+        # split the document
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size, 
+            chunk_overlap=self.chunk_overlap
+        )
+        chunks = text_splitter.split_documents(documents)
+        print(f"Split into {len(chunks)} chunks of text (max. {self.chunk_size} tokens each)")
+        return chunks
+
+
+    def build_embeddings(self):
+        """
+        Create embeddings and save them in a Chroma vector store
+        Returns the indexed db
+        """
+        chunks = self.chunk_data()
+        print("\n\n\nChunking complete...\n")
+        print(f"{len(chunks)} chunks were created.\n")
+        print(f"Creating embedding. May take some minutes...")
+
+        if self.database_type == 'chroma':
+            if self.use_client:
+                # reset client
+                self.db_client.reset()
+                # create a collection
+                collection = self.db_client.get_or_create_collection(
+                    name=self.collection_name, 
+                    embedding_function=self.embeddings,
+                )
+                # add text to the collection
+                for doc in chunks:
+                    collection.add(
+                        ids=[str(uuid.uuid1())], 
+                        metadatas=doc.metadata, 
+                        documents=doc.page_content
+                    )
+
+                # instantiate chroma db
+                vector_store = self.get_vector_store()
+
+            else:
+                # create vector store from documents using OpenAIEmbeddings
+                vector_store = Chroma.from_documents(
+                    chunks, 
+                    self.embeddings, 
+                    persist_directory=persist_directory, 
+                )
+        else:
+            s3_bucket = None
+            # if aws_enable:
+            #     session = boto3.Session(
+            #         aws_access_key_id=aws_access_key,
+            #         aws_secret_access_key=aws_secret_key
+            #     )
+            #     s3 = session.resource('s3')
+            #     s3_bucket = s3.Bucket(aws_bucket_name)
+            #     self.fetch_aws_documents(source_directory, s3_bucket)
+
+            vector_store = FAISS.from_documents(chunks, self.embeddings)
+            vector_store.save_local(persist_directory)
+            # if aws_enable:
+            #     self.upload_database(persist_directory, s3_bucket)
+            print(f"Ingestion complete!")
+        return vector_store
+
+
+    def get_vector_store(self):
+        """
+        Returns the existing vector store
+        """
+        if self.database_type == 'chroma':
+            if self.use_client:
+                vector_store = Chroma(
+                    client=self.db_client,
+                    collection_name=self.collection_name,
+                    embedding_function=self.embeddings,
+                    persist_directory=persist_directory, 
+                )
+            else:
+                vector_store = Chroma(
+                    persist_directory=persist_directory,
+                    embedding_function=self.embeddings,
+                )
+        else:
+            vector_store = FAISS.load_local(persist_directory, self.embeddings)
+        return vector_store
+
+
+    def fetch_aws_documents(self, source_dir, source_bucket):
+
+        print("fetching aws documents")
+
+        for s3_object in source_bucket.objects.all():
+            path, filename = os.path.split(s3_object.key)
+            if verbose:
+                print(f"downloading {path}/{filename}")
+            source_bucket.download_file(s3_object.key, os.path.join(source_dir, filename))
+
+
+    def upload_database(self, db_dir):
+
+        print("upload_database")
+        all_files = []
+        for ext in ["faiss", "pkl"]:
+            files = glob.glob(os.path.join(db_dir, f"**/*{ext}"), recursive=False)
+            all_files.extend(files)
+
+
+
+
