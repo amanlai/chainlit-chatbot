53c86938f6a8e9d423c72b3d06452fca74a16a8a
amanlai <manlai.lp@gmail.com>
2024-03-02 01:11:52 -0800

    modifications needed for the heroku side

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: chainlit run chatbot.py --host 0.0.0.0 --port $PORT
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # --host and --port when running chainlit run ....
 # nohup ./run.sh &> chainlit-chatbot.log &
 # nohup chainlit run app.py &> chainlit-chatbot.log &
-chainlit run chatbot.py
+chainlit run chatbot.py --host 0.0.0.0 --port $PORT
